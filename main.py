e767a62c6504d103f20a41fa24debfbae15db674
Sergey <sergey030901@mail.ru>
2021-04-17 09:59:24 +0300

    -

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 import sys
-from sign import Ui_Registration_Or_Sign
-from mainMenu import MainMenu
+
 import hashlib
 import web3
-import a
+import datetime
 
+
+import a
+from sign import Ui_Registration_Or_Sign
+from main_menu import Ui_MainWindow
+from Change_vod_ud import ChangeVod
 
 class Sign(QtWidgets.QMainWindow):
     def __init__(self):
@@ -52,14 +56,55 @@ class Sign(QtWidgets.QMainWindow):
             self.eror.setWindowTitle("Ошибка входа")
             self.eror.showMessage("Данные введены неверно.")
         
+
+
         
+class MainMenu(QtWidgets.QMainWindow):
+    def __init__(self, Cont=None):
+        QtWidgets.QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.eror= QtWidgets.QErrorMessage()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.change_vod_ud)
+        #self.Cont = Cont
+        #self.adrus = web3.Web3.toChecksumAddress("0xb62b0d39a824fab6fb7e5915c59fe3a779765ef2")
+        #self.Cont = a.Contract(self.adrus)
 
 
+    
+    def change_vod_ud(self):
+        self.open = ChangeUd(self.Cont)
+        self.open.show()      
+
+
+class ChangeUd(QtWidgets.QMainWindow):
+    def __init__(self, Cont=None):
+        super().__init__()
+        self.ui = ChangeVod()
+        self.eror= QtWidgets.QErrorMessage()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.savedan)
+        self.Cont = Cont
+
+
+    def savedan(self):
+        number = int(self.ui.lineEdit.text())
+        data = self.ui.lineEdit_2.text().split('.')
+        dt = datetime.datetime(day=int(data[0]), month=int(data[1]), year=int(data[2]))
+        dt = int(dt.timestamp())        
+        kateg = int(self.ui.lineEdit_3.text())
+        ans = self.Cont.add_dr_pass(number, dt, kateg)
+        if ans == 'Ошибка регистрации страховки':
+            self.eror.setWindowTitle("Ошибка входа")
+            self.eror.showMessage("Данные введены неверно.")
+        else:
+            self.close()
+    
 
 
 if __name__ == "__main__":
     app=QtWidgets.QApplication([])
     myapp = Sign()
+    #myapp = MainMenu()
     myapp.show()
     sys.exit(app.exec())
-
